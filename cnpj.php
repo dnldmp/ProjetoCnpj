e8efbb6449b7b39b3835db09bf2211142bd47ed4
dnldmp <danilodomp@gmail.com>
2018-01-03 23:44:22 -0200

    Criando function para chamar por cnpj

--- cnpj.php
+++ cnpj.php
@@ -1,47 +1,59 @@
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<form class="form-group" method="POST" enctype="multipart/form-data">
+	<label for="nome">Arquivo:</label>
+	<input type="file" name="fileUpload" class="form-control-file"><br>
+	<button class="btn btn-success" type="submit">Enviar</button>
+</form>
+
 <?php 
+//FEITO COM AMOR PELO DANILO DOMINONI
 
-	$cnpj = "33014556000196";
-	$link = "https://www.receitaws.com.br/v1/cnpj/$cnpj";
+	consultaCNPJ("19861350000413");
+	function consultaCNPJ($cod){
+		$cnpj = $cod;
+		$link = "https://www.receitaws.com.br/v1/cnpj/$cnpj";
 
-	$ch = curl_init($link);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		$ch = curl_init($link);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
-	$response = curl_exec($ch);
+		$response = curl_exec($ch);
 
-	curl_close($ch);
+		curl_close($ch);
 
-	$data = json_decode($response, true);
+		$data = json_decode($response, true);
 
-	$headers = array();
+		$headers = array();
 
-	foreach ($data as $key => $value) {
-		array_push($headers, ucfirst($key));
-	}
-
-	$file = fopen("empresas.csv", "w+");
-
-	fwrite($file, implode(",", $headers) . "\r\n");
-
-	$dataRow = array();
-
-	foreach ($data as $key => $value) {
-
-		if (!is_array($value)){
-
-			array_push($dataRow, $value);
-
-		} else {
-
-			array_push($dataRow, "Vazio");
+		foreach ($data as $key => $value) {
+			array_push($headers, ucfirst($key));
 		}
 
+		$file = fopen("empresas.csv", "w+");
+
+		fwrite($file, implode(",", $headers) . "\r\n");
+
+		$dataRow = array();
+
+		foreach ($data as $key => $value) {
+
+			if (!is_array($value)){
+
+				array_push($dataRow, $value);
+
+			} else {
+
+				array_push($dataRow, "Vazio");
+			}
+
+		}
+
+		fwrite($file, implode(",", $dataRow) . "\r\n");
+
+		fclose($file);
+
+		echo "Arquivo criado com sucesso!";
+
 	}
-
-	fwrite($file, implode(",", $dataRow) . "\r\n");
-
-	fclose($file);
-
-	echo "Arquivo criado com sucesso!";
 
  ?>
